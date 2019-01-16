7c635d487e46b6392f0f8ad7df4286e03678f050
Joel Savitz <jsavitz@redhat.com>
2019-01-16 10:40:39 -0500

    sysinfo.sh now writes to local logfile

--- dev/sysinfo.sh
+++ dev/sysinfo.sh
@@ -6,7 +6,8 @@
 # TODO: Save data to file
 
 E_INCORRECT_USAGE=1
-VERSION=1
+VERSION=1.1
+LOG_FILE=sysinfo."$(date +%F-%H)".log
 
 usage() {
 	echo "Usage: `basename $0` "
@@ -18,13 +19,15 @@ hello_there() {
 }
 
 display_info() {
-  printf "===========[when]============\n"
-  printf "%s\n" "$(date "+%F %T")\n"
-  printf "==========[users]============\n"
-  who
-  printf "=======[forhowlong]==========\n"
-  uptime
-  printf "===========[end]=============\n"
+  printf "The following output will be written to: %s\n" $LOG_FILE
+  touch $LOG_FILE
+  printf "===========[when]============\n" | tee -a $LOG_FILE
+  printf "%s\n" "$(date "+%F %T")" 	   | tee -a $LOG_FILE
+  printf "==========[users]============\n" | tee -a $LOG_FILE
+  who 					   | tee -a $LOG_FILE
+  printf "=======[forhowlong]==========\n" | tee -a $LOG_FILE
+  uptime 				   | tee -a $LOG_FILE
+  printf "===========[end]=============\n" | tee -a $LOG_FILE
 }
 
 # Main entry point
--- prod/sysinfo.sh
+++ prod/sysinfo.sh
@@ -3,9 +3,11 @@
 # By Joel Savitz <joelsavitz@gmail.com>
 #
 # Purpose: Display some information about the system
+# TODO: Save data to file
 
 E_INCORRECT_USAGE=1
-VERSION=1
+VERSION=1.1
+LOG_FILE=sysinfo."$(date +%F-%H)".log
 
 usage() {
 	echo "Usage: `basename $0` "
@@ -17,17 +19,18 @@ hello_there() {
 }
 
 display_info() {
-  printf "===========[when]============\n"
-  printf "%s\n" "$(date "+%F %T")\n"
-  printf "==========[users]============\n"
-  who
-  printf "=======[forhowlong]==========\n"
-  uptime
-  printf "===========[end]=============\n"
+	printf "%s\n" $LOG_FILE
+  touch $LOG_FILE
+  printf "===========[when]============\n" | tee -a $LOG_FILE
+  printf "%s\n" "$(date "+%F %T")" 	   | tee -a $LOG_FILE
+  printf "==========[users]============\n" | tee -a $LOG_FILE
+  who 					   | tee -a $LOG_FILE
+  printf "=======[forhowlong]==========\n" | tee -a $LOG_FILE
+  uptime 				   | tee -a $LOG_FILE
+  printf "===========[end]=============\n" | tee -a $LOG_FILE
 }
 
 # Main entry point
 hello_there
 display_info
-
 exit
