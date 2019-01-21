bd8fe81b10ca82a4818082561b6b4a1855833a43
Joel Savitz <joelsavitz@gmail.com>
2019-01-21 17:09:24 -0500

    add plans to README.md and plans files, create delete.sh

--- README.md
+++ README.md
@@ -21,20 +21,30 @@ install.sh has what you need
 	* looks for <filename> in $SCIRPT_DIR/../dev
 	* opens that file for writing in vim (this will be configurable)
 	* fails if the file does not exist
+	* Option -l | --local to use execution directory // TODO
 * deploy.sh <filename>
 	* copies <filename> from $SCRIPT_DIR/../dev to $SCRIPT_DIR
+	* appends scripts to appropriate headers // TODO
 	* script is now executable via PATH
 	* fails if the file does not exist
+	* runs tests on script // TODO
+	* fails if tests fail and reports failures // TODO
+	* fails if requirements for sytem to run script are not met
+* delete.sh <filename>
+	* deletes <filename> from $SCRIPT_DIR and $SCRIPT_DIR/../dev
+	* will ask for confirmation
+	* fails if the file does not exist
+
 
 ##### PROPOSED FEATURES FOLLOW:
 * disable.sh <filename>
 	* deletes <filename> from $SCRIPT_DIR
 	* script is no longer executable via PATH
 	* fails if the file does not exist
-* delete.sh <filename>
-	* deletes <filename> from $SCRIPT_DIR and $SCRIPT_DIR/../dev
-	* will ask for confirmation
-	* fails if the file does not exist
+* import.sh <library-name>
+	* source a set of files in order to access a particular API
+
+dbprint function: will print when script is run in $/dev dir only
 
 The other scripts are basically just toys for now
 
---
+++ prod/delete.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+# delete.sh v1 created 21 January 2019 
+# By Joel Savitz <joelsavitz@gmail.com>
+#
+# Purpose: Deletes scripts and associated files
+
+SUCCESS=0
+E_INCORRECT_USAGE=1
+E_FILE_NOT_FOUND=2
+
+usage() {
+	echo "Usage: `basename $0` <>"
+	exit $E_INCORRECT_USAGE
+}
+
+hello_there() {
+	printf "Hello there, user. I am  `basename $0` v%s\n" $VERSION
+}
+
+get_confirmation() {
+	printf "$1"
+	read -p "Are you sure? [Yy/cancel] " -n 1 -r
+	echo
+	if [[ ! $REPLY =~ ^[Yy]$ ]]
+	then
+		echo "Delete operation cancelled"
+		exit $SUCCESS
+	fi
+
+}
+
+# Does the first argument exist?
+check_args() {
+	if [ -z "$1" ]
+	then
+		usage
+	fi
+}
+
+check_if_not_exists() {
+	if [ ! -f $ABSOLUTE_PATH ]
+	then
+		printf "Script file \"$1\" does not exist. Cannot delete.\n"
+		exit $E_FILE_NOT_FOUND
+	fi
+}
+
+delete_script() {
+	rm -rf "$SCRIPT_DIR/../dev/$1"
+
+	# Remove the script from the prod/ directory if it has been deployed
+	if [ -f "$SCRIPT_DIR/$1" ]
+	then
+		rm -rf "$SCRIPT_DIR/$1"
+	fi
+
+	echo "Script $1 deleted successfully"
+}
+
+# Main entry point for script
+hello_there
+
+# Determine the absolute file path
+ABSOLUTE_PATH="$SCRIPT_DIR/../dev/$1"
+
+# Validate preconditions for main body execution
+
+# User must supply one or more arguments
+check_args  $1
+
+# Script file must exist
+check_if_not_exists $1
+
+# Quit if user does not want to continue
+get_confirmation "You have commanded me to delete $1\n"
+
+# Delete the script and associated files
+delete_script $1
+
+exit
